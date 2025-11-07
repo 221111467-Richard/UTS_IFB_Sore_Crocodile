93031f336520ccd62d88c64b7611324d7b12ad11
Kenzie Pragata <221110403@students.mikroskil.ac.id>
2025-11-07 16:51:57 +0700

    Menambahkan fitur preprocessing dan handling stopword serta inisialisasi 2 variabel last_df dan wordcloud_img

--- project-root/app.py
+++ project-root/app.py
@@ -17,5 +17,32 @@ app = Flask(__name__)
 sentiment_model = joblib.load("model/sentiment_svm.pkl")
 vectorizer = joblib.load("model/vectorizer.pkl")
 
+stop_factory = StopWordRemoverFactory()
+stopwords = set(stop_factory.get_stop_words())
+
+negation_words = {
+    "tidak", "tak", "tiada", "bukan", "jangan", "belum", "tanpa", "enggak", "gak", "ndak", "nda", "kagak", "ga",
+    "ora", "dak", "idak", "ndok", "sing", "endak", "keneh", "ulun", "kada", "tara", "hamo", "takkan",
+    "janganlah", "bukannya", "nggak", "nggak usah", "ga usah", "ga mau", "sama sekali tidak",
+    "sama sekali bukan", "mana mungkin", "mustahil"
+}
+
+filtered_stopwords = [w for w in stopwords if w not in negation_words]
+stopwords = set(filtered_stopwords)
+
+stem_factory = StemmerFactory()
+stemmer = stem_factory.create_stemmer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z\s]', ' ', text)
+    tokens = text.split()
+    tokens = [stemmer.stem(w) for w in tokens if w not in stopwords]
+    return " ".join(tokens)
+
+
+last_df = None
+wordcloud_img = None
+
 if __name__ == "__main__":
     app.run(debug=True)
