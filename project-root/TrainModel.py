29611db9e7401ad77af034f4a22b3019ce8f9044
221111467 Richard <richardmailchat@gmail.com>
2025-11-07 14:38:58 +0700

    Membuat model SVM yang akan digunakan untuk analisis sentimen nanti

--- project-root/TrainModel.py
+++ project-root/TrainModel.py
@@ -1,4 +1,20 @@
 import joblib
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report, accuracy_score
 
 vectorizer = joblib.load("model/vectorizer.pkl")
 X_train, y_train, X_test, y_test = joblib.load("model/data_split.pkl")
+
+
+model = LinearSVC()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("🔎 Evaluasi Model SVM")
+print("Akurasi:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+
+joblib.dump(model, "model/sentiment_svm.pkl")
+print("✅ Model SVM berhasil dilatih & disimpan di model/sentiment_svm.pkl")
